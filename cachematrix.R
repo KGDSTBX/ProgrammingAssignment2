e06a7f147891b8eaeaa3f36149ca1756fe099885
KGFeMan <krishnagopalaraman@gmail.com>
2014-04-23 19:57:17 +0530

    committing cachematrix.R before pusing to remote

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The function(s) creates an inverse of a matrix ( assumed to be invertible)
+## To esure optimum use if resources this value is then cached
 
-## Write a short comment describing this function
+## This function creates matrix and can give information including inverse of the matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<- function(x = matrix(...) ){
+    ## initializing the inv(matrix) to be cached
+    inv <- NULL
+    ## setting the value of the matrix
+    set <- function(y) {
+        x <<- matrix(y,nrow=2,ncol=2)
+        inv <<- NULL
+    }
+    ## getting the value of the matrix
+    get <- function() x
+    ## setting the inverse of the matrix 
+    setinverse <- function(solve) inv <<- solve
+    ## getting the inverse of the matrix 
+    getinverse <- function() inv
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## This function calculates inverse of a matrix if not available in cache or return value from cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    ## checking if inverse is already calculated
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    ## if Inverse not already calculated then calculating inverse
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    inv
 }
